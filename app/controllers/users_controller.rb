d3f53529c570eec62b95dab1d8edbea43a2cf9f4
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2014-08-25 23:04:39 -0300

    Adicionado o UserController. Imcompleto

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+   def new
+      @user = User.new
+   end
+
+   def create
+      @user = User.new(params[user_params])
+      if @user.save
+         redirect_to @user, notice: 'Cadastro criado com sucesso!'
+      else
+         render action: :new
+      end
+   end
+
+   private
+   def user_params
+      params.require(:user).permit(:email, :full_name, :location, :password, :password_confirmation, :bio)
+   end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ActiveRecord::Base
 
    validates_presence_of :email, :full_name, :location, :password
    validates_confirmation_of :password
-   validates_length_of :bio, minimum: 30 allow_blank: false
+   #validates_length_of :bio, minimum: 30 allow_blank: false
    validates_uniqueness_of :email
 
    validate do
-      errors.add(:email, :invalid) unless
-      email.match(EMAIL_REGEXP)
+     errors.add(:email,:invalid) unless email.match(EMAIL_REGEXP)
    end
 end
